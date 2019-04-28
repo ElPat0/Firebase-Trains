966eeeeb0371412ca62aa845974a6f816b2a77e0
Patrick Owens <manbearpat@gmail.com>
2019-04-28 13:34:56 -0700

    corrected firebase link

--- assets/js/java.js
+++ assets/js/java.js
@@ -10,4 +10,40 @@
   };
   firebase.initializeApp(config);
 
+  var database = firebase.database();
+
+
+  // 2. Button for adding Employees
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
   
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var firstDepart = moment($("#firstDepart-input").val().trim(), "HH:mm").format("X");
+    var trainFreq = $("#frequency-input").val().trim();
+  
+    // Creates local "temporary" object for holding employee data
+    var newTrain = {
+      name: trainName,
+      destination: trainDest,
+      firstDeparture: firstDepart,
+      frequency: trainFreq
+    };
+    // Uploads employee data to the database
+  database.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.firstDeparture);
+  console.log(newTrain.frequency);
+
+  //alert("New train successfully added");
+
+  // Clears all of the text-boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#firstDepart-input").val("");
+  $("#frequency-input").val("");
+});
