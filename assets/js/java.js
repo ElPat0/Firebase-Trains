63e9f3f34d6d935bf40d3d0b490f8cd8e4b54ab4
Patrick Owens <manbearpat@gmail.com>
2019-04-28 12:40:49 -0700

    linked firebase, jquery, moment.js

---
+++ assets/js/java.js
@@ -0,0 +1,13 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDIFUThU0RP9s6nXciNqnkXhl2eq9tvT0k",
+    authDomain: "class-firebase-exercise.firebaseapp.com",
+    databaseURL: "https://class-firebase-exercise.firebaseio.com",
+    projectId: "class-firebase-exercise",
+    storageBucket: "class-firebase-exercise.appspot.com",
+    messagingSenderId: "165662759876"
+  };
+  firebase.initializeApp(config);
+
+  
